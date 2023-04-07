e3fd49e14800d0aece17d97595efdb85db03e170
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-07 17:50:45 -0300

    :gear: create: config - Fish

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+#alias nvm "nvm.exe"
+
+  alias ll "exa -l -g --icons --tree --level=2"
+  alias la "ll -a"
+  alias lst "exa -l -g --icons"
+  alias lsta "lst -a"
