5f3776402da0dcd55759ddfa39d1c5ab73f4be44
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-10 10:58:16 -0300

    [ :zzz: ] | setup: LazyVim (terminal::nvim)

---
+++ LazyVim/lua/plugins/coding.lua
@@ -0,0 +1,26 @@
+return {
+  -- Incremental rename
+  {
+    "smjonas/inc-rename.nvim",
+    cmd = "IncRename",
+    config = true,
+  },
+
+  -- Refactoring tool
+  {
+    "ThePrimeagen/refactoring.nvim",
+    keys = {
+      {
+        "<leader>r",
+        function()
+          require("refactoring").select_refactor()
+        end,
+        mode = "v",
+        noremap = true,
+        silent = true,
+        expr = false,
+      },
+    },
+    opts = {},
+  },
+}
---
+++ LazyVim/lua/plugins/colorscheme.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      }
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
