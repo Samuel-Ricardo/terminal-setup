e506e868ae5765e717e6a6ad60928b7f68d0014d
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-01-27 11:09:35 -0300

    [ :art: ] | update: style - theme (app::style)

--- LazyVim/lua/config/lazy.lua
+++ LazyVim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.elixir" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 
     -- NOTE: add langs
@@ -44,7 +45,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "catppuccin", "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
